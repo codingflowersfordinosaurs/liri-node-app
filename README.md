b6c9867f676b861a227f9ce78ada71f5926e0a3a
Plant Lady <codeplantsbylouie@gmail.com>
2019-03-14 11:55:45 -0400

    initial changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # liri-node-app
- LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+- LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+### LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies
+- LIRI is like iPhone's SIRI
+- - While SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Inerpretation and Recognition Interface
+- LIRI will be a command line node app that takes in parameters and gives you back data
+
+## LIRI Functions
+### LIRI will:
+- Search Spotify for songs, Bands in Town for concerts, and OMDB for movies
+
+## How to retrieve the data that will power the app
+- You will need to send request using the AXIOS package to the Bands in Town, Spotify, and OMDB APIs
+- - These Node packages are crucial for this project
---
+++ liri.js
@@ -0,0 +1,115 @@
+// VARIABLES + KEYS(for the API stuff)
+require("dotenv").config();
+
+var keys = require("./keys");
+
+var spotify = new spotify(keys.spotify);
+
+// need to to do process.argv[2] and process.argv[3]: because need user input and their pick of band/concert, song/artist, and movie pick
+var userInput = process.argv[2];
+var userPick = process.argv[3];
+
+// SWITCH: FOR COMMANDS AND FUNCTIONS
+switch (userInput) {
+  case "concert-this":
+    showConcert(userPick);
+    break;
+
+  case "spotify-this-song":
+    showSpotify(userPick);
+    break;
+
+  case "movie-this":
+    showMovie(userPick);
+    break;
+
+  case "do-what-it-says":
+    doIt();
+    break;
+}
+
+// FUNCTIONS
+function showConcert(userPick) {
+
+}
+
+function showSpotify(userPick) {
+
+  // If user doesn't input an artist and song then this will be the one to show up
+  if (!userPick) {
+    userPick = "Cher Believe";
+  }
+
+  spotify.search({
+    type: 'track',
+    query: userPick,
+  }, 
+  function (err, data) {
+    if (err) {
+      console.log("Error occurred: " + err);
+    } else {
+      // console.log (JSON/stringify(data));
+      console.log("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆");
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name);
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+      console.log("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆");
+    }
+  });
+
+} // end of function showSpotify(userPick)
+
+function showMovie(userPick) {
+  console.log(userPick);
+
+  // if user doesn't pick a movie, then this will show up
+  if (!userPick) {
+    userPick = "Mr. Nobody";
+  }
+
+  // need to connect to the OMDB with the API key
+  var queryURL = "http://www.omdbapi.com/?t=" + userPick + "&y-&y=plot=short&apikey=trilogy";
+  console.log(queryURL); //
+
+  request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆");
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotton Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country Produced: " + JSON.parse(body).Country);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      console.log("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆");
+    }
+  });
+
+} // end of function showMovie(userPick)
+
+function doIt() {
+  fs.readFile("random.txt", "UTF8", function(error, data) {
+    
+    if(error) {
+      console.log(error);
+    }
+    console.log(data);
+
+    var dataArr = data.split(", ");
+    var it = dataArr[0];
+    var pick = dataArr[1];
+
+    if (it === "spotify-this-song") {
+      userPick = pick;
+      showSpotify(userPick);
+    } else if (it === "movie-this") {
+      userPick = pick;
+      showMovie(userPick);
+    } else if (it === "concert-this") {
+      userPick = pick;
+      showConcert(userPick);
+    }
+
+  });
+}
